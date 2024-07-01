746659cd47858f9c94004698c6b61de48534f933
seungh0 <will.seungho@gmail.com>
2024-07-01 21:52:39 +0900

    통합 테스트 환경 세팅

---
+++ cadio-core/src/test/java/kr/hakdang/cadio/BaseTest.java
@@ -0,0 +1,14 @@
+package kr.hakdang.cadio;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * BaseTest
+ *
+ * @author seungh0
+ * @since 2024-07-01
+ */
+@ExtendWith(SpringExtension.class)
+public abstract class BaseTest {
+}
---
+++ cadio-web/src/test/java/kr/hakdang/cadio/web/BaseTest.java
@@ -0,0 +1,16 @@
+package kr.hakdang.cadio.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * BaseTest
+ *
+ * @author seungh0
+ * @since 2024-07-01
+ */
+@Slf4j
+@ExtendWith(SpringExtension.class)
+public abstract class BaseTest {
+}
