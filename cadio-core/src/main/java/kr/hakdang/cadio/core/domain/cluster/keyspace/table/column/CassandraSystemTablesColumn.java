bfbf2870a3050d77c4e6a70aefa812abd640f8b3
Seungho Kang <will.seungho@gmail.com>
2024-07-03 00:15:02 +0900

    Renaming CassandraSystemKeyspace/Table/Column (#9)

---
+++ cadio-core/src/main/java/kr/hakdang/cadio/core/domain/cluster/keyspace/table/column/CassandraSystemTablesColumn.java
@@ -0,0 +1,27 @@
+package kr.hakdang.cadio.core.domain.cluster.keyspace.table.column;
+
+import kr.hakdang.cadio.core.domain.cluster.keyspace.table.CassandraSystemTable;
+import lombok.Getter;
+
+/**
+ * CassandraSystemTablesColumn
+ *
+ * @author seungh0
+ * @since 2024-07-02
+ */
+@Getter
+public enum CassandraSystemTablesColumn {
+
+    TABLES_KEYSPACE_NAME(CassandraSystemTable.SYSTEM_SCHEMA_TABLES, "keyspace_name"),
+    TABLES_TABLE_NAME(CassandraSystemTable.SYSTEM_SCHEMA_TABLES, "table_name"),
+    ;
+
+    private final CassandraSystemTable table;
+    private final String columnName;
+
+    CassandraSystemTablesColumn(CassandraSystemTable table, String columnName) {
+        this.table = table;
+        this.columnName = columnName;
+    }
+
+}
