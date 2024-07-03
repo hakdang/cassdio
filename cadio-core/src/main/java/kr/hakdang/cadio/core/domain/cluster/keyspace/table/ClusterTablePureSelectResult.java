ace69b0f6ce1e25ffebe2fad925d647ab3d540e3
Geunee <akageun@gmail.com>
2024-07-03 21:26:52 +0900

    Pure Select Table 로직 추가 (#19)

--- cadio-core/src/main/java/kr/hakdang/cadio/core/domain/cluster/keyspace/table/ClusterTablePureSelectResult.java
+++ cadio-core/src/main/java/kr/hakdang/cadio/core/domain/cluster/keyspace/table/ClusterTablePureSelectResult.java
@@ -1,10 +1,34 @@
 package kr.hakdang.cadio.core.domain.cluster.keyspace.table;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * ClusterTablePureSelectResult
  *
  * @author akageun
  * @since 2024-06-30
  */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClusterTablePureSelectResult {
+    private boolean wasApplied;
+    private List<String> columnNames;
+    private List<Map<String, Object>> rows;
+    private String previewToken;
+    private String nextToken;
+
+    @Builder
+    public ClusterTablePureSelectResult(boolean wasApplied, List<String> columnNames, List<Map<String, Object>> rows, String previewToken, String nextToken) {
+        this.wasApplied = wasApplied;
+        this.columnNames = columnNames;
+        this.rows = rows;
+        this.previewToken = previewToken;
+        this.nextToken = nextToken;
+    }
 }
