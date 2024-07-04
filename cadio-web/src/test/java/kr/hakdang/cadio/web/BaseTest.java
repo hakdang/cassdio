d6a97ec5c01b41bb64c7af7e708815fa72b25b96
Seungho Kang <will.seungho@gmail.com>
2024-07-05 01:06:33 +0900

    Profile Setup & default spring config (#28)
    * setup application spring config
    
    * Setup Profile
    
    * 프로파일 정책 TODO

--- cadio-web/src/test/java/kr/hakdang/cadio/web/BaseTest.java
+++ cadio-web/src/test/java/kr/hakdang/cadio/web/BaseTest.java
@@ -2,6 +2,7 @@ package kr.hakdang.cadio.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
  * @since 2024-07-01
  */
 @Slf4j
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 public abstract class BaseTest {
 }
