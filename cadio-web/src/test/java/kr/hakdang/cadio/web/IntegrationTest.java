4833cbbdbccd414caf5c268291e27c7a6fc89f74
Seungho Kang <will.seungho@gmail.com>
2024-07-06 22:22:47 +0900

    Integration Test 분리 (#37)

--- cadio-web/src/test/java/kr/hakdang/cadio/web/IntegrationTest.java
+++ cadio-web/src/test/java/kr/hakdang/cadio/web/IntegrationTest.java
@@ -1,6 +1,7 @@
 package kr.hakdang.cadio.web;
 
 import kr.hakdang.cadio.CoreRoot;
+import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @author seungh0
  * @since 2024-07-01
  */
+@Tag("integration-test")
 @SpringBootTest(classes = {CoreRoot.class})
 public abstract class IntegrationTest extends BaseTest {
 }
