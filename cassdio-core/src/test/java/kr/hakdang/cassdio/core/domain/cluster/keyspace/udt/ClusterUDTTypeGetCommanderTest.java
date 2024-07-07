8dd0d142c20b7ff6d853bfe7ff26feb4dd51add2
Seungho <will.seungho@gmail.com>
2024-07-07 10:58:47 +0900

    UDT Type 테스트 추가

---
+++ cassdio-core/src/test/java/kr/hakdang/cassdio/core/domain/cluster/keyspace/udt/ClusterUDTTypeGetCommanderTest.java
@@ -0,0 +1,69 @@
+package kr.hakdang.cassdio.core.domain.cluster.keyspace.udt;
+
+import kr.hakdang.cassdio.IntegrationTest;
+import kr.hakdang.cassdio.core.domain.cluster.keyspace.udt.ClusterUDTTypeArgs.ClusterUDTTypeGetArgs;
+import kr.hakdang.cassdio.core.domain.cluster.keyspace.udt.ClusterUDTTypeException.ClusterUDTTypeNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * ClusterUDTTypeGetCommanderTest
+ *
+ * @author Seungho Kang (will.seungho@webtoonscorp.com)
+ * @version 1.0.0
+ * @since 2024. 07. 07.
+ */
+class ClusterUDTTypeGetCommanderTest extends IntegrationTest {
+
+    @Autowired
+    private ClusterUDTTypeGetCommander clusterUDTTypeGetCommander;
+
+    @Test
+    void get_udt_type_in_keyspace() {
+        // given
+        ClusterUDTTypeGetArgs args = ClusterUDTTypeGetArgs.builder()
+            .keyspace(keyspaceName)
+            .type("test_type_1")
+            .build();
+
+        // when
+        ClusterUDTType sut = clusterUDTTypeGetCommander.getType(makeSession(), args);
+
+        // then
+        assertThat(sut.getTypeName()).isEqualTo("test_type_1");
+        assertThat(sut.getColumns()).hasSize(3);
+        assertThat(sut.getColumns()).containsEntry("field_1", "text");
+        assertThat(sut.getColumns()).containsEntry("field_2", "bigint");
+        assertThat(sut.getColumns()).containsEntry("field_3", "time");
+    }
+
+    @Test
+    void when_get_not_exists_udt_type_in_keyspace_throw_not_exists_exception() {
+        // given
+        ClusterUDTTypeGetArgs args = ClusterUDTTypeGetArgs.builder()
+            .keyspace(keyspaceName)
+            .type("not_exist_type")
+            .build();
+
+        // when
+        assertThatThrownBy(() -> clusterUDTTypeGetCommander.getType(makeSession(), args))
+            .isInstanceOf(ClusterUDTTypeNotFoundException.class);
+    }
+
+    @Test
+    void when_get_not_exists_udt_type_in_keyspace_throw_not_exists_exception_2() {
+        // given
+        ClusterUDTTypeGetArgs args = ClusterUDTTypeGetArgs.builder()
+            .keyspace("another_keyspace")
+            .type("test_type_1")
+            .build();
+
+        // when & then
+        assertThatThrownBy(() -> clusterUDTTypeGetCommander.getType(makeSession(), args))
+            .isInstanceOf(ClusterUDTTypeNotFoundException.class);
+    }
+
+}
---
+++ cassdio-core/src/test/java/kr/hakdang/cassdio/core/domain/cluster/keyspace/udt/ClusterUDTTypeListCommanderTest.java
@@ -0,0 +1,59 @@
+package kr.hakdang.cassdio.core.domain.cluster.keyspace.udt;
+
+import kr.hakdang.cassdio.IntegrationTest;
+import kr.hakdang.cassdio.core.domain.cluster.keyspace.udt.ClusterUDTTypeArgs.ClusterUDTTypeListArgs;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * ClusterUDTTypeListCommanderTest
+ *
+ * @author Seungho Kang (will.seungho@webtoonscorp.com)
+ * @version 1.0.0
+ * @since 2024. 07. 07.
+ */
+class ClusterUDTTypeListCommanderTest extends IntegrationTest {
+
+    @Autowired
+    private ClusterUDTTypeListCommander clusterUDTTypeListCommander;
+
+    @Test
+    void list_udt_types_in_keyspace() {
+        // given
+        ClusterUDTTypeListArgs args = ClusterUDTTypeListArgs.builder()
+            .keyspace(keyspaceName)
+            .nextPageState(null)
+            .build();
+
+        // when
+        ClusterUDTTypeListResult sut = clusterUDTTypeListCommander.listTypes(makeSession(), args);
+
+        // then
+        assertThat(sut.getTypes()).hasSize(1);
+        assertThat(sut.getNextPageState()).isNull();
+        assertThat(sut.getTypes().getFirst().getTypeName()).isEqualTo("test_type_1");
+        assertThat(sut.getTypes().getFirst().getColumns()).hasSize(3);
+        assertThat(sut.getTypes().getFirst().getColumns()).containsEntry("field_1", "text");
+        assertThat(sut.getTypes().getFirst().getColumns()).containsEntry("field_2", "bigint");
+        assertThat(sut.getTypes().getFirst().getColumns()).containsEntry("field_3", "time");
+    }
+
+    @Test
+    void when_empty_type_exist_in_keyspace_return_empty_list() {
+        // given
+        ClusterUDTTypeListArgs args = ClusterUDTTypeListArgs.builder()
+            .keyspace("empty_keyspace")
+            .nextPageState(null)
+            .build();
+
+        // when
+        ClusterUDTTypeListResult sut = clusterUDTTypeListCommander.listTypes(makeSession(), args);
+
+        // then
+        assertThat(sut.getTypes()).isEmpty();
+        assertThat(sut.getNextPageState()).isNull();
+    }
+
+}
