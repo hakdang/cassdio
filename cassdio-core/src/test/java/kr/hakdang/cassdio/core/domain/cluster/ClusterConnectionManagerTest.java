ad7f687aee503e38c827af1dfbbb3f7abc197fe4
akageun <akageun@gmail.com>
2024-07-18 23:50:33 +0900

    Cluster Manage
    - File -> MapDB
    - UI Refactoring

--- cassdio-core/src/test/java/kr/hakdang/cassdio/core/domain/cluster/ClusterConnectionManagerTest.java
+++ cassdio-core/src/test/java/kr/hakdang/cassdio/core/domain/cluster/ClusterConnectionManagerTest.java
@@ -1,16 +1,11 @@
 package kr.hakdang.cassdio.core.domain.cluster;
 
 import kr.hakdang.cassdio.IntegrationTest;
-import kr.hakdang.cassdio.core.domain.cluster.info.ClusterInfoManager;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
 class ClusterConnectionManagerTest extends IntegrationTest {
-
-    @Autowired
-    private ClusterInfoManager clusterInfoManager;
 
     @Test
     void registerTest() {
