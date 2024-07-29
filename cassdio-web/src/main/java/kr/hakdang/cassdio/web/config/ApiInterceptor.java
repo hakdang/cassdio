d33ba62ad0d74731b554866c54534a6d7dd416a1
akageun <akageun@gmail.com>
2024-07-29 23:42:09 +0900

    Apply API Context

--- cassdio-web/src/main/java/kr/hakdang/cassdio/web/config/ApiInterceptor.java
+++ cassdio-web/src/main/java/kr/hakdang/cassdio/web/config/ApiInterceptor.java
@@ -2,12 +2,15 @@ package kr.hakdang.cassdio.web.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.hakdang.cassdio.web.config.context.CassdioContext;
+import kr.hakdang.cassdio.web.config.context.CassdioContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.HandlerMapping;
 
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * ApiInterceptor
@@ -22,11 +25,18 @@ public class ApiInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //Map<String, String> pathVariables = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+
+        CassdioContextHolder.set(
+            CassdioContext.builder()
+                .requestId(UUID.randomUUID().toString())
+                .build()
+        );
+
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        CassdioContextHolder.clear();
     }
 }
