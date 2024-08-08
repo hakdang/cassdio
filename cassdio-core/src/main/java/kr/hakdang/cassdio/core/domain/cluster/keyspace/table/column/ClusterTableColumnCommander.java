ac3a75b8ea8d596594e5cc3739baaccb175168fc
akageun <akageun@gmail.com>
2024-08-09 00:21:20 +0900

    Query Editor Column List
    - apply sorting(with partition, clustering key)

--- cassdio-core/src/main/java/kr/hakdang/cassdio/core/domain/cluster/keyspace/table/column/ClusterTableColumnCommander.java
+++ cassdio-core/src/main/java/kr/hakdang/cassdio/core/domain/cluster/keyspace/table/column/ClusterTableColumnCommander.java
@@ -3,6 +3,7 @@ package kr.hakdang.cassdio.core.domain.cluster.keyspace.table.column;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.querybuilder.BuildableQuery;
 import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
 import com.datastax.oss.driver.api.querybuilder.select.Select;
 import com.datastax.oss.driver.api.querybuilder.select.SelectFrom;
@@ -17,9 +18,11 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.bindMarker;
 import static java.util.Collections.emptyList;
@@ -41,18 +44,24 @@ public class ClusterTableColumnCommander extends BaseClusterCommander {
     public CqlSessionSelectResults columnList(String clusterId, String keyspace, String table, List<String> columnList) {
         CqlSession session = cqlSessionFactory.get(clusterId);
 
-        SimpleStatement statement;
-
         Select select = getColumnTable(session, keyspace)
             .all()
             .whereColumn(CassandraSystemTablesColumn.TABLES_KEYSPACE_NAME.getColumnName()).isEqualTo(bindMarker())
             .whereColumn(CassandraSystemTablesColumn.TABLES_TABLE_NAME.getColumnName()).isEqualTo(bindMarker());
 
-//        if (CollectionUtils.isNotEmpty(columnList)) {
-//            select.whereColumn("column_name").in(columnList.stream().map(info -> bindMarker()).toList());
-//        }
+        List<String> arr = new ArrayList<>();
+        arr.add(keyspace);
+        arr.add(table);
 
-        statement = select.build(keyspace, table)
+        if (CollectionUtils.isNotEmpty(columnList)) {
+            select = select.whereColumn("column_name").in(columnList.stream()
+                .map(info -> bindMarker())
+                .collect(Collectors.toSet()));
+
+            arr.addAll(columnList);
+        }
+
+        SimpleStatement statement = select.build(arr.toArray())
             .setTimeout(Duration.ofSeconds(3));
 
         ResultSet resultSet = session.execute(statement);
